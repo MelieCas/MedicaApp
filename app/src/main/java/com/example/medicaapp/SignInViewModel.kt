2adaddb5146bcc35f923752ad5952ec2a33a638a
Mélie Casares <vcasares5@gmail.com>
2025-05-08 17:29:01 +0200

    Public funtions

--- app/src/main/java/com/example/medicaapp/SignInViewModel.kt
+++ app/src/main/java/com/example/medicaapp/SignInViewModel.kt
@@ -10,10 +10,16 @@ class SignInViewModel : ViewModel() {
     private var _Nom: String = ""
     private var _Email: String = ""
     private var _Password: String = ""
+    private var _EmailRegexString: String = "\\w+@(gmail.com|hotmail.com)"
+    private var _EmailRegex = Regex.fromLiteral(_EmailRegexString)
 
     private var _BannedWords: ArrayList<String> = arrayListOf("Puto", "Caca")
     private val _ErrorUser = MutableLiveData<String>("")
+    private val _ErrorEmail = MutableLiveData<String>("")
+    private val _ErrorPassword = MutableLiveData<String>("")
     val errorUser: LiveData<String> = _ErrorUser
+    val errorEmail: LiveData<String> = _ErrorEmail
+    val errorPassword: LiveData<String> = _ErrorPassword
 
     fun setUsuari(usuari: String) {
         _Usuari = usuari
@@ -31,16 +37,30 @@ class SignInViewModel : ViewModel() {
         _Password = password
     }
 
-    private fun checkUser() {
-        if (_Usuari.isEmpty()) {
+    public fun checkUser() {
+        if (_Usuari.isBlank()) {
             _ErrorUser.value = "El nom d'usuari no pot estar buit"
         } else if (_BannedWords.contains(_Usuari)) {
             _ErrorUser.value = "El nom d'usuari no pot ser despectiu"
+        } else if (_Usuari.length <= 2) {
+            _ErrorUser.value = "El nom d'usuari es molt curt"
+        } else if (_Usuari.length > 25) {
+            _ErrorUser.value = "El nom d'usuari es molt llarg"
         }
     }
 
-    private fun chechEmail() {
+    public fun chechEmail() {
+        if (_Email.isBlank()) {
+            _ErrorEmail.value = "El email no pot estar buit"
+        } else if (!_EmailRegex.matches(_Email)) {
+            _ErrorEmail.value = "El format del Gmail no es correcte"
+        }
+    }
 
+    public fun checkPassword() {
+        if (_Password.isBlank()) {
+            _ErrorPassword.value = "La contrasenya no pot ser buida"
+        }
     }
 
 }
