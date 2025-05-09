9855a2a2b7f4e37085501f940c53964bab3c69de
Mélie Casares <vcasares5@gmail.com>
2025-05-09 18:00:25 +0200

    View model and register

--- app/src/main/java/com/example/medicaapp/SignInActivity.kt
+++ app/src/main/java/com/example/medicaapp/SignInActivity.kt
@@ -1,16 +1,112 @@
 package com.example.medicaapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.ViewModel
 
 class SignInActivity : AppCompatActivity() {
+
+    private val model: SignInViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_in)
+
+        val usuariText = findViewById<EditText>(R.id.user2)
+        val nomText = findViewById<EditText>(R.id.name)
+        val emailText = findViewById<EditText>(R.id.email)
+        val passwordText = findViewById<EditText>(R.id.password2)
+        val confirmPasswordText = findViewById<EditText>(R.id.password)
+        val signInButton = findViewById<Button>(R.id.sign_in2)
+
+        usuariText.addTextChangedListener {
+            model.setUsuari(it.toString())
+        }
+        usuariText.setOnFocusChangeListener { v, hasFocus ->
+            model.checkUser()
+            if(!model.errorUser.value.equals("")) {
+                usuariText.setError(model.errorUser.value)
+            } else {
+                usuariText.setError(null)
+            }
+        }
+
+        nomText.addTextChangedListener {
+            model.setNom(it.toString())
+        }
+        nomText.setOnFocusChangeListener { v, hasFocus ->
+            model.checkNom()
+            if(!model.errorName.value.equals("")) {
+                nomText.setError(model.errorName.value)
+            } else {
+                nomText.setError(null)
+            }
+        }
+
+        emailText.addTextChangedListener {
+            model.setEmail(it.toString())
+        }
+
+        emailText.setOnFocusChangeListener { v, hasFocus ->
+            model.checkEmail()
+            if(!model.errorEmail.value.equals("")) {
+                emailText.setError(model.errorEmail.value)
+            } else {
+                emailText.setError(null)
+            }
+        }
+
+        passwordText.addTextChangedListener {
+            model.setPassword(it.toString())
+        }
+
+        passwordText.setOnFocusChangeListener { v, hasFocus ->
+            model.checkPassword()
+            if(!model.errorPassword.value.equals("")) {
+                passwordText.setError(model.errorPassword.value)
+            } else {
+                passwordText.setError(null)
+            }
+        }
+
+        confirmPasswordText.addTextChangedListener {
+            model.setConfirmPassword(it.toString())
+        }
+
+        confirmPasswordText.setOnFocusChangeListener { v, hasFocus ->
+            model.checkConfirmPassword()
+            if(!model.errorConfirmPassword.value.equals("")) {
+                confirmPasswordText.setError(model.errorConfirmPassword.value)
+            } else {
+                confirmPasswordText.setError(null)
+            }
+        }
+
+        signInButton.setOnClickListener {
+
+            if (model.validForm()) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                Toast.makeText(this, "Registre completat", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Dades erroneas", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/medicaapp/SignInViewModel.kt
+++ app/src/main/java/com/example/medicaapp/SignInViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.medicaapp
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.beust.jcommander.internal.Console
 import java.util.Locale
 
 class SignInViewModel : ViewModel() {
@@ -11,23 +13,27 @@ class SignInViewModel : ViewModel() {
     private var _Nom: String = ""
     private var _Email: String = ""
     private var _Password: String = ""
-    private var _EmailRegexString: String = "\\w+@(gmail.com|hotmail.com)"
-    private var _EmailRegex = Regex.fromLiteral(_EmailRegexString)
-    private var _PasswordRegexString: String = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\S+\$).{8,20}"
-    private var _PasswordRegex = Regex.fromLiteral(_PasswordRegexString)
+    private var _ConfirmPassword: String = ""
+    private var _EmailRegexString: String = "^[A-Za-z0-9._%+-]+@(gmail\\.com|hotmail\\.com)$"
+    private var _EmailRegex = Regex(_EmailRegexString)
+    private var _PasswordRegexString: String = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\S+\\$).{8,20}$"
+    private var _PasswordRegex = Regex(_PasswordRegexString)
 
     private var _BannedWords: ArrayList<String> = arrayListOf("puto", "caca")
     private val _ErrorUser = MutableLiveData<String>("")
     private val _ErrorEmail = MutableLiveData<String>("")
     private val _ErrorPassword = MutableLiveData<String>("")
-    private val _ErrorName = MutableLiveData<String>()
+    private val _ErrorName = MutableLiveData<String>("")
+    private val _ErrorConfirmPassword = MutableLiveData<String>("")
     val errorUser: LiveData<String> = _ErrorUser
     val errorEmail: LiveData<String> = _ErrorEmail
     val errorPassword: LiveData<String> = _ErrorPassword
     val errorName: LiveData<String> = _ErrorName
+    val errorConfirmPassword: LiveData<String> = _ErrorConfirmPassword
 
     fun setUsuari(usuari: String) {
         _Usuari = usuari
+
     }
 
     fun setNom(nom: String) {
@@ -42,6 +48,10 @@ class SignInViewModel : ViewModel() {
         _Password = password
     }
 
+    fun setConfirmPassword(confirmPassword: String) {
+        _ConfirmPassword = confirmPassword;
+    }
+
     public fun checkUser() {
         if (_Usuari.isBlank()) {
             _ErrorUser.value = "El nom d'usuari no pot estar buit"
@@ -51,14 +61,18 @@ class SignInViewModel : ViewModel() {
             _ErrorUser.value = "El nom d'usuari es molt curt"
         } else if (_Usuari.length > 25) {
             _ErrorUser.value = "El nom d'usuari es molt llarg"
+        } else {
+            _ErrorUser.value = ""
         }
     }
 
-    public fun chechEmail() {
+    public fun checkEmail() {
         if (_Email.isBlank()) {
             _ErrorEmail.value = "El email no pot estar buit"
         } else if (!_EmailRegex.matches(_Email)) {
             _ErrorEmail.value = "El format del Gmail no es correcte"
+        } else {
+            _ErrorEmail.value = ""
         }
     }
 
@@ -67,6 +81,8 @@ class SignInViewModel : ViewModel() {
             _ErrorPassword.value = "La contrasenya no pot ser buida"
         } else if (!_PasswordRegex.matches(_Password)) {
             _ErrorPassword.value = "La contrasenya no es molt segura"
+        } else {
+            _ErrorPassword.value = ""
         }
     }
 
@@ -79,6 +95,25 @@ class SignInViewModel : ViewModel() {
             _ErrorName.value = "El nom es molt llarg"
         } else if (_BannedWords.contains(_Nom.lowercase(Locale.getDefault()))) {
             _ErrorName.value = "El nom no pot ser ofensiu"
+        } else {
+            _ErrorName.value = ""
         }
+    }
+
+    public fun checkConfirmPassword() {
+        if (!_ConfirmPassword.equals(_Password)) {
+            _ErrorConfirmPassword.value = "No es la mateixa contrasenya"
+        } else {
+            _ErrorConfirmPassword.value = ""
+        }
+    }
+
+    public fun validForm(): Boolean {
+        if (_ErrorName.value.equals("") && _ErrorUser.value.equals("") && _ErrorEmail.value.equals("") && _ErrorPassword.value.equals("") && _ErrorConfirmPassword.value.equals("")) {
+            return true;
+        } else {
+            return false
+        }
+
     }
 }
